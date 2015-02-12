0907320786f66c4d08c92a7bd44d11af51a9697a
Markus Peröbner <markus.peroebner@gmail.com>
2015-02-12 22:36:47 +0100

    trains to pretty well circles

--- circle-training.py
+++ circle-training.py
@@ -10,22 +10,22 @@ MAX_TURNS = 1000
 MAX_EPOCHS = 100
 
 def main():
-    net = shortcuts.buildNetwork(2, 3, 1, hiddenclass=structure.TanhLayer, bias=False)
+    net = shortcuts.buildNetwork(2, 3, 3, 3, 3, 3, 1, hiddenclass=structure.TanhLayer, bias=True)
 
     for turn in xrange(MAX_TURNS):
         print 'Running turn %s...' % (turn, )
         
-        write_net_to_image('activation_%03d.png' % (turn, ), net)
-
         ds = generate_tranining_ds()
         trainer = trainers.BackpropTrainer(net, ds)
 
         for epoch in xrange(MAX_EPOCHS):
             mse = trainer.train()
 
+        write_net_to_image('activation_%03d.png' % (turn, ), net, ds)
+
         print 'Last MSE %s' % (mse, )
 
-def generate_tranining_ds(sample_count=100): 
+def generate_tranining_ds(sample_count=1000):
     ds = datasets.SupervisedDataSet(2, 1)
 
     for i in xrange(sample_count):
@@ -42,7 +42,7 @@ def generate_tranining_ds(sample_count=100):
 
     return ds
 
-def write_net_to_image(image_path, net, image_size=(100, 100)):
+def write_net_to_image(image_path, net, ds, image_size=(100, 100)):
     width, height = image_size
     
     img = PIL.Image.new('L', image_size)
@@ -53,6 +53,12 @@ def write_net_to_image(image_path, net, image_size=(100, 100)):
             net_out = net.activate((x / float(width) * 2.0 - 1.0, y / float(height) * 2.0 - 1.0))
 
             pixels[x, y] = net_out_to_pixel(net_out[0])
+
+    #for net_in, net_out in ds:
+    #    x = min(width, max(0, (net_in[0] + 1.0) / 2.0 * width))
+    #    y = min(height, max(0, (net_in[1] + 1.0) / 2.0 * width))
+    #
+    #    pixels[x, y] = net_out_to_pixel(net_out[0])
 
     img.save(image_path, 'PNG')
 
